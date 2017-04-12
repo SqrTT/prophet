3fa14abd2923caffd200d102bec0e0cc62a665f4
a.obitskyi <tolik@sqrtt.pro>
2017-04-12 17:53:43 +0300

    fix initial configuration

--- src/extension.ts
+++ src/extension.ts
@@ -7,30 +7,27 @@
 import * as vscode from 'vscode';
 
 const initialConfigurations = {
-	version: '0.0.1',
+	version: '0.1.0',
 	configurations: [
 		{
-			type: 'mock',
-			request: 'launch',
-			name: 'Mock Debug',
-			program: '${workspaceRoot}/readme.md',
-			stopOnEntry: true
+			"type": "prophet",
+			"request": "launch",
+			"name": "Attach to Sandbox",
+			"hostname": "*.demandware.net",
+			"username": "<username>",
+			"password": "<password>",
+			"codeversion": "version1",
+			"cartridgeroot": "auto",
+			"workspaceroot": "${workspaceRoot}"
 		}
 	]
 };
 
 export function activate(context: vscode.ExtensionContext) {
 
-	context.subscriptions.push(vscode.commands.registerCommand('extension.prophet.getProgramName', config => {
-		return vscode.window.showInputBox({
-			placeHolder: "Please enter the name of a markdown file in the workspace folder",
-			value: "readme.md"
-		});
-	}));
-
 	context.subscriptions.push(vscode.commands.registerCommand('extension.prophet.provideInitialConfigurations', () => {
 		return [
-			'// Use IntelliSense to learn about possible Mock debug attributes.',
+			'// Use IntelliSense to learn about possible Prophet attributes.',
 			'// Hover to view descriptions of existing attributes.',
 			JSON.stringify(initialConfigurations, null, '\t')
 		].join('\n');
