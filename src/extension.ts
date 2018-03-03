64b69e2cb40d4d447db7d88e1e395d9479a9300a
Georgi Kirilov <gkkirilov@gmail.com>
2018-03-03 22:29:32 +0200

    feature/open-documentation: Add command to open documentation or xchange

--- src/extension.ts
+++ src/extension.ts
@@ -243,3 +243,11 @@ function createIsmlLanguageServer(context : ExtensionContext, configuration : Wo
 export function deactivate() {
 	// nothing to do
 }
+
+commands.registerCommand('extension.prophet.command.open.documentation', () => {
+	commands.executeCommand('vscode.open', Uri.parse('https://documentation.demandware.com'));
+});
+
+commands.registerCommand('extension.prophet.command.open.xchange', () => {
+	commands.executeCommand('vscode.open', Uri.parse('https://xchange.demandware.com'));
+});
